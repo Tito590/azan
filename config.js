56febab60a012630ce2df7cefd1f27cd161a3252
Tito590 <cenditito@gmail.com>
2026-09-01 17:37:33 +0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const CONFIG = {
-  panoramaUrl: "https://raw.githubusercontent.com/Tito590/azan/main/assets/ChatGPT%20Image%20Sep%201%2C%202026%2C%2004_47_41%20PM.png",
-  pouchUrl: "https://raw.githubusercontent.com/Tito590/azan/main/assets/argopuro-pouch-solid.png",
-  frames: ['https://raw.githubusercontent.com/Tito590/azan/main/assets/ChatGPT%20Image%20Sep%201%2C%202026%2C%2001_49_59%20PM.png', 'https://raw.githubusercontent.com/Tito590/azan/main/assets/ChatGPT%20Image%20Sep%201%2C%202026%2C%2001_56_19%20PM.png', 'https://raw.githubusercontent.com/Tito590/azan/main/assets/ChatGPT%20Image%20Sep%201%2C%202026%2C%2001_58_15%20PM.png', 'https://raw.githubusercontent.com/Tito590/azan/main/assets/ChatGPT%20Image%20Sep%201%2C%202026%2C%2002_01_11%20PM.png', 'https://raw.githubusercontent.com/Tito590/azan/main/assets/ChatGPT%20Image%20Sep%201%2C%202026%2C%2002_06_01%20PM.png'],
-  frameDuration: 180,
-  greeting: "Terima Kasih sudah membeli kopi dari kami, karenamu kami terus berjuang demi kopi terbaik Indonesia.",
-  shopUrl: "#"
-};
